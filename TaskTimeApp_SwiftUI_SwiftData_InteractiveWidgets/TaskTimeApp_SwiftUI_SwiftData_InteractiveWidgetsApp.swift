1331955ec4448becd077c978470ff08116de1c72
Elcieren <elcierenn@gmail.com>
2024-11-23 21:10:55 +0300

    Initial Commit

---
+++ TaskTimeApp_SwiftUI_SwiftData_InteractiveWidgets/TaskTimeApp_SwiftUI_SwiftData_InteractiveWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTimeApp_SwiftUI_SwiftData_InteractiveWidgetsApp.swift
+//  TaskTimeApp_SwiftUI_SwiftData_InteractiveWidgets
+//
+//  Created by Eren Elçi on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskTimeApp_SwiftUI_SwiftData_InteractiveWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
